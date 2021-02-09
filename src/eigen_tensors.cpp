7f23bdc61cc449fce15c08665ea8fe42ec4729cb
Matt Landreman <mattland@umd.edu>
2021-02-09 12:29:35 -0500

    Now passing a tensor from C++ to python

--- src/eigen_tensors.cpp
+++ src/eigen_tensors.cpp
@@ -1,6 +1,9 @@
 #include <Eigen/Core>
+#include <unsupported/Eigen/CXX11/Tensor>
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
+
+namespace py = pybind11;
 
 int add(int j, int k) {
   return j + k;
@@ -27,10 +30,25 @@ Matrix increment_matrix(Matrix& m1) {
   return m2;
 }
 
-namespace py = pybind11;
+py::array_t<double> make_tensor3() {
+  Eigen::Tensor<double, 3> t(2, 3, 4);
+  double index = 0.0;
+  for (int j = 0; j < t.dimension(0); j++) {
+    for (int k = 0; k < t.dimension(1); k++) {
+      for (int l = 0; l < t.dimension(2); l++) {
+	t(j, k, l) = index;
+	index += 0.1;
+      }
+    }
+  }
+
+  // To return the tensor to python, we must convert the Eigen::Tensor to py::array_t:
+  return py::array_t<double, py::array::f_style>(t.dimensions(), t.data());
+}
 
 PYBIND11_MODULE(eigen_tensors, m) {
   m.def("add", &add, "A function that adds.");
   m.def("increment_vector", &increment_vector, "Add 1 to each element of a vector.");
   m.def("increment_matrix", &increment_matrix, "Add 1 to each element of a matrix.");
+  m.def("make_tensor3", &make_tensor3, "Create a rank-3 tensor.");
 }
