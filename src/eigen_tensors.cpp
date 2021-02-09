afa8466cd780135e331d7613cc2433ac971b38c7
Matt Landreman <mattland@umd.edu>
2021-02-09 13:43:47 -0500

    Now passing tensors both ways

--- src/eigen_tensors.cpp
+++ src/eigen_tensors.cpp
@@ -1,13 +1,12 @@
+// For interfacing ND Eigen Tensors with pybind11, see
+// See https://github.com/pybind/pybind11/issues/1377
+
 #include <Eigen/Core>
 #include <unsupported/Eigen/CXX11/Tensor>
 #include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
 
 namespace py = pybind11;
-
-int add(int j, int k) {
-  return j + k;
-}
 
 typedef Eigen::ArrayXd Vector;
 typedef Eigen::MatrixXd Matrix;
@@ -32,12 +31,12 @@ Matrix increment_matrix(Matrix& m1) {
 
 py::array_t<double> make_tensor3() {
   Eigen::Tensor<double, 3> t(2, 3, 4);
-  double index = 0.0;
+  double val = 0.0;
   for (int j = 0; j < t.dimension(0); j++) {
     for (int k = 0; k < t.dimension(1); k++) {
       for (int l = 0; l < t.dimension(2); l++) {
-	t(j, k, l) = index;
-	index += 0.1;
+	t(j, k, l) = val;
+	val += 0.1;
       }
     }
   }
@@ -46,9 +45,41 @@ py::array_t<double> make_tensor3() {
   return py::array_t<double, py::array::f_style>(t.dimensions(), t.data());
 }
 
+// See
+// https://pybind11.readthedocs.io/en/stable/advanced/pycpp/numpy.html
+// "py::array::f_style | py::array::forcecast" ensures the incoming
+// data has the same layout as Eigen's default layout.
+py::array_t<double> modify_tensor3(py::array_t<double, py::array::f_style | py::array::forcecast> in_array) {
+  
+  // Check # of dimensions: see https://pybind11.readthedocs.io/en/stable/advanced/pycpp/numpy.html
+  py::buffer_info buf = in_array.request();
+  if (buf.ndim != 3) {
+    throw std::runtime_error("Number of dimensions must be 3.");
+  }
+  
+  // Without copying, represent the incoming numpy array as an Eigen::Tensor:
+  Eigen::TensorMap<Eigen::Tensor<double, 3>> t1(static_cast<double*>(buf.ptr),
+						buf.shape[0], buf.shape[1], buf.shape[2]);
+
+  // Make a new tensor of the same shape:
+  Eigen::Tensor<double, 3> t2(buf.shape[0], buf.shape[1], buf.shape[2]);
+  double val = 0.0;
+  for (int j = 0; j < t2.dimension(0); j++) {
+    for (int k = 0; k < t2.dimension(1); k++) {
+      for (int l = 0; l < t2.dimension(2); l++) {
+	t2(j, k, l) = t1(j, k, l) + val;
+	val += 0.1;
+      }
+    }
+  }
+
+  // To return the tensor to python, we must convert the Eigen::Tensor to py::array_t:
+  return py::array_t<double, py::array::f_style>(t2.dimensions(), t2.data());
+}
+
 PYBIND11_MODULE(eigen_tensors, m) {
-  m.def("add", &add, "A function that adds.");
   m.def("increment_vector", &increment_vector, "Add 1 to each element of a vector.");
   m.def("increment_matrix", &increment_matrix, "Add 1 to each element of a matrix.");
   m.def("make_tensor3", &make_tensor3, "Create a rank-3 tensor.");
+  m.def("modify_tensor3", &modify_tensor3, "Add 0.1 * index to each element of a rank-3 tensor.");
 }
--- tests/test_eigen_tensors.py
+++ tests/test_eigen_tensors.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import unittest
+import numpy as np
 import eigen_tensors as et
 
 class MainTest(unittest.TestCase):
@@ -8,12 +9,38 @@ class MainTest(unittest.TestCase):
         t = et.make_tensor3();
         self.assertEqual(t.shape, (2, 3, 4))
 
-        index = 0.0
+        val = 0.0
         for j in range(2):
             for k in range(3):
                 for l in range(4):
-                    self.assertAlmostEqual(t[j, k, l], index)
-                    index += 0.1
+                    self.assertAlmostEqual(t[j, k, l], val)
+                    val += 0.1
+        print("# of tests:", int(val * 10))
 
+    def test_modify_tensor3(self):
+        index = 0
+        for d1 in range(4):
+            for d2 in range(4):
+                for d3 in range(4):
+                    t1 = np.random.rand(d1, d2, d3)
+                    t2 = et.modify_tensor3(t1)
+                    val = 0.0
+                    for j in range(d1):
+                        for k in range(d2):
+                            for l in range(d3):
+                                """
+                                print('{} {} {} - {} {} {} - {} {} {}'.format(d1, d2, d3,
+                                                                           j, k, l,
+                                                                              t1[j, k, l],
+                                                                              t2[j, k, l],
+                                                                              val))
+                                """
+                                self.assertAlmostEqual(t2[j, k, l],
+                                                       t1[j, k, l] + val)
+                                val += 0.1
+                                index += 1
+                                
+        print("# of tests:", index)
+                    
 if __name__ == '__main__':
     unittest.main()
